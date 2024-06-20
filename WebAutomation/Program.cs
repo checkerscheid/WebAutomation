80fdd82d550bdd079470dbf810bfb591f3d578c1
Christian Scheid <168387787+checkerscheid@users.noreply.github.com>
2024-06-21 01:37:19 +0200

    Update Program.cs

--- WebAutomation/Program.cs
+++ WebAutomation/Program.cs
@@ -31,7 +31,7 @@ namespace WebAutomation {
 		/// <summary></summary>
 		public static WebAutomationServer MainProg;
 		public static string myName;
-		public const string subversion = "110";
+		public const string subversion = "111";
 		private static TextWriterTraceListener trtl;
 		private static System.Timers.Timer tlog;
 		/// <summary>
